6171aa7269fcaaffae00adb1b89dbd60cef69b9b
Mike <93550477+himikeorg@users.noreply.github.com>
2025-06-05 13:35:16 -0400

    Add button to new divs

--- app.js
+++ app.js
@@ -14,15 +14,37 @@ function addBookToLibrary(title, author, pages, read) {
 }
 
 // Loop through array, display each book
+//Let's try if loop to only create the ones that exist.
+
+// NEXT UP - make the div disapper when it's erased from myLibrary (in the for loop below)
+
 function displayBooks() {
     for (const element of myLibrary) {
-        const newDiv = document.createElement("div");
-        const newContent = document.createTextNode(element.title + " by " + element.author);
-        newDiv.appendChild(newContent);
-        document.querySelector("body").appendChild(newDiv);
-    }
+        if (document.querySelector(`div[data-id="${element.id}"`) === null) {
+            const newDiv = document.createElement("div");
+            const newBtn = document.createElement("button");
+            const newContent = document.createTextNode(element.title + " by " + element.author);
+            newBtn.textContent = "Click me";
+            newDiv.dataset.id = element.id;
+            newBtn.dataset.id = element.id;
+            newBtn.addEventListener("click", (e) => {
+                // Loop through array and remove the item whose data.id matches the button data.id.
+                for (let i = 0; i < myLibrary.length; i++) {
+                    if (e.target.dataset.id === myLibrary[i].id) {
+                        myLibrary.splice(i, 1);
+                        // document.getElementById(e.target.id)
+                        console.log(myLibrary);
+                    }
+                }
+            });
+            newDiv.appendChild(newContent);
+            document.querySelector("body").appendChild(newDiv);
+            document.querySelector(`[data-id="${element.id}"`).appendChild(newBtn);
+        };
+    };
 }
 
+   
 // New Book button opens modal dialog
 // ADD closing the modal as well.
 
@@ -33,7 +55,8 @@ newBook.addEventListener("click", () => {
     dialog.showModal();
 });
 
-// Stop submit button from submitting form
+// Stop button from submitting form, add book with input values,
+// reset form, close dialog.
 
 const btn = document.querySelector("#btn");
 btn.addEventListener("click", (event) => {
@@ -45,9 +68,10 @@ btn.addEventListener("click", (event) => {
     addBookToLibrary(title, author, pages, read);
     document.getElementById("book-form").reset();
     dialog.close();
+    displayBooks();
 });
 
-// Next problem 
-// How do I take the values from the form to create books?
+// Current problem - on first iteration, there's no div to remove. 
+// Maybe check output of div on first iteration, and make if statement?
 
 
